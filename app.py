9812e72d882b04a925ba5f072bf778c6f3e7d142
NFRamz <133985045+NFRamz@users.noreply.github.com>
2025-02-04 02:17:35 -0800

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ def download_youtube_video():
             'merge_output_format': 'mp3',
             'outtmpl': os.path.join(DOWNLOAD_FOLDER, '%(title)s.%(ext)s'),
             'quiet': True,
-            'postprocessor_args': ['-ffmpeg-location', '/usr/bin/ffmpeg']
-            'geo_bypass': True
+            'postprocessor_args': ['-ffmpeg-location', '/usr/bin/ffmpeg'],
+            'geo_bypass': True,
         }
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
