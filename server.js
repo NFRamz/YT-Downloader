13bfc87b86430027cff938b1370ce2d9a2977232
NFRamz <133985045+NFRamz@users.noreply.github.com>
2025-02-14 16:16:24 -0800

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 });
 
 // ✅ Mengambil environment variables untuk API & Google Analytics ID
-app.get('/.websitenfdownloderbynframz512025copyright15022025', (req, res) => {
+app.get('/nfdownloderbynframz512025copyright15022025', (req, res) => {
     res.json({
         YOUTUBE_MP3_API: process.env.YOUTUBE_MP3_API,
         YOUTUBE_MP4_API: process.env.YOUTUBE_MP4_API,
@@ -29,7 +29,7 @@ app.get('/.websitenfdownloderbynframz512025copyright15022025', (req, res) => {
 });
 
 // ✅ Port dari `.env` atau default 3000
-const PORT = process.env.PORT || e3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`🚀 Server berjalan di http://localhost:${PORT}`);
 });
