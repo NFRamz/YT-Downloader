2554c55daeba4bfd6e5649d5e07148c11c279460
NFRamz <133985045+NFRamz@users.noreply.github.com>
2026-02-11 18:54:22 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,15 +6,13 @@ const app = express();
 
 app.use(cors());
 
-// ✅ Melayani file statis (HTML, CSS, gambar, dll.)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// ✅ Menyajikan halaman utama dari `public/index.html`
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// ✅ Mengambil environment variables untuk API & Google Analytics ID
 app.get('/89asdsdasad89w02129pejdni10348k0mkg9pbmg8m42822fnnauf97ai91jmn2a8a2475nfo43pa9w719dnd86nq8weh67w7ber8jrji2nkqhg9e823j7hf8jendjhqkgedjr78drd8d9jqpourjndbumdadijubo1jnfi1kjf8rb181', (req, res) => {
     res.json({
         YOUTUBE_MP3_API: process.env.YOUTUBE_MP3_API,
@@ -28,8 +26,8 @@ app.get('/89asdsdasad89w02129pejdni10348k0mkg9pbmg8m42822fnnauf97ai91jmn2a8a2475
     });
 });
 
-// ✅ Port dari `.env` atau default 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`🚀 Server berjalan di http://localhost:${PORT}`);
 });
+
